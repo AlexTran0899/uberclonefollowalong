139251ff1c745b28820e1a922f86e8288c1497aa
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-11-11 11:48:31 -0600

    half way throught he project just learned that you need to clear the caches for new env file and changes in the bable config

--- App.js
+++ App.js
@@ -4,11 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen'
+import { GOOGLE_MAPS_APIKEY } from "@env"
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{ headerShown: false }} />
+            <Stack.Screen name='MapScreen' component={MapScreen} options={{ headerShown: false }} />
+
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native'
+import { Icon } from 'react-native-elements'
 import tw from 'tailwind-react-native-classnames'
 const data = [
     {
@@ -17,19 +19,22 @@ const data = [
 ]
 
 const navOptions = () => {
+    const navigation = useNavigation()
     return (
         <FlatList
             data={data}
             horizontal
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <TouchableOpacity 
-                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                     <View>
                         <Image
                             style={{ width: 120, height: 120, resizeMode: 'contain' }}
                             source={{ uri: item.image }} />
-                        <Text style={tw`mt-2 textlg font-semibold`}>{item.title}</Text>
+                        <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon style={tw`p-2 bg-black w-10 rounded-full mt-4`} name='arrowright' color='white' type='antdesign' />
                     </View>
                 </TouchableOpacity>
             )} />
